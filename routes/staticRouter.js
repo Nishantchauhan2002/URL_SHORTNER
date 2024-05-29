db3a5dedc78293f973d80aabe4cd9a547c866949
Nishant Chauhan <nishant031102@gmail.com>
2024-05-29 13:57:06 +0530

    Null conditon added

--- routes/staticRouter.js
+++ routes/staticRouter.js
@@ -5,6 +5,10 @@ const router = express.Router();
 router.get("/", async (req, res) => {
   const allurls = await URL.find({});
 
+  if (!allurls) {
+    return res.json({ err: "No data found" });
+  }
+
   return res.render("home", {
     urls: allurls,
   });
